e1e2a6172819f6b6245e25c08b7a9e92503153e6
Fabala DEMBELE <dembele.fabala@gmail.com>
2023-05-05 12:42:04 +0200

    chore: css

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,65 +1,52 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useMemo, useState } from "react";
 import mqtt from 'mqtt';
 import './App.css';
-import {useSelector} from "react-redux";
-import {useFirestoreConnect} from "react-redux-firebase";
-import {HorizontalGridLines, LineSeries, MarkSeries, VerticalGridLines, XAxis, XYPlot, YAxis} from "react-vis";
-import {Card, CardContent, CardMedia, Chip, Grid, Typography} from "@material-ui/core";
 
 function App() {
 
-  const [datas, setDatas] = useState([]);
+  const [dataList, setDataList] = useState([]);
   const [isActive, setIsActive] = useState(false);
-  const mainTopic = "/alarm-iot/activate";
+  let newValue = false;
+  const baseTopic = "/alarm-iot";
+  const activateTopic = baseTopic + "/activate";
+  const alertTopic = baseTopic + "/alert";
 
-  const toggleChange = () => setIsActive(value => !value);
+  const toggleChange = () => {
+      setIsActive(!isActive);
+      newValue = isActive;
+      sendMessage(activateTopic , { activate: newValue });
+  };
 
-  // useFirestoreConnect(() => [{collection: "sensors/5190/samples", storeAs: "samples", where:['date', '<', Date.now()], }])
-  useFirestoreConnect(() => [{
-    collection: "sensors/0013a20041a72961/samples",
-    storeAs: "samples",
-    // limit: 10,
-    // orderBy: ["date", "desc"]
-    // where: ['date', '>', Date.now()- (1000 * 60*10)]
-  }])
-  const samples = useSelector((state) => state.firestore.ordered.samples)
 
-  useEffect(() => {
-/*    console.log(samples?.length)
-    if (samples)
-      {
-        const slice = samples
-          .map((sample) => ({x: sample?.date, y: sample?.value.toString()}))
-          .slice(samples?.length - 50, samples?.length - 1);
-        console.log(slice)
-        setDatas(slice)
-      }*/
-    const { mqttClient, sendMessage } = setupMqttClient();
-    setTimeout(() => {
-      sendMessage(mainTopic, { test: 'LUBIN VIENT D\'UN ISSEKAI' });
-    });
+  const updateDataList = (newData) => {
+    setDataList((prevDataList) => [...prevDataList, newData]);
+  };
 
-    return () => {
-      mqttClient.end();
-    };
-  }, [samples])
-
-  const isEmpty = datas?.pop()?.y <= 100;
-  const currentImage = isEmpty ?
-    'https://i.pinimg.com/originals/71/b5/9d/71b59de71b5d34b464d8838bf7be70f1.jpg' :
-    'https://www.drronsanimalhospitalsimivalley.com/wp-content/uploads/2018/11/Happy-Cat-Eating-Food.jpg'
-
-  const setupMqttClient = () => {
+  function setupMqttClient () {
     const mqttClient = mqtt.connect('wss://test.mosquitto.org:8081');
 
     mqttClient.on('connect', () => {
       console.log('Connected to MQTT broker');
-      mqttClient.subscribe(mainTopic);
+      mqttClient.subscribe(activateTopic + '/on' );
+      mqttClient.subscribe(activateTopic + '/off' );
+      mqttClient.subscribe(alertTopic );
     });
     mqttClient.on('message', (topic, message) => {
       const payload = JSON.parse(message.toString());
-      console.log(`Received message on ${topic}:`, payload);
-      // Process the received message and update your state here
+      switch (topic){
+        case (activateTopic+'/on' ):
+          console.log(`Received message on ${topic}:`, payload.toString());
+          updateDataList(payload.toString());
+          break;
+          case (activateTopic+'/off'):
+          console.log(`Received message on ${topic}:`, payload.toString());
+            updateDataList(payload.toString());
+            break;
+        case(alertTopic):
+          console.log(`Received message on ${topic}:`, payload.toString());
+          updateDataList(<span className="alert">{payload.toString()}</span>);
+
+      }
     });
 
     mqttClient.on('error', (error) => {
@@ -68,74 +55,32 @@ function App() {
 
     const sendMessage = (topic, message) => {
       const payload = JSON.stringify(message);
+      console.log(`--> Send message on ${topic}:`, message);
       mqttClient.publish(topic, payload);
     };
-
     return { mqttClient, sendMessage };
   };
+
+  const { mqttClient, sendMessage } = setupMqttClient();
+
 
   return (
     <div className="App">
 
-      <header className="App-header">
-        <p style={{
-          backgroundColor: 'rgba(255,255,255,0.5)',
-          color: "deeppink",
-          padding: 50,
-          borderRadius: 20,
-        }}>Ma Gamelle Connectée
 
-
-        </p>
-        {isEmpty ?
-          <Chip label={'Bowl is empty'} color={"secondary"}/> :
-          <Chip label={'Bowl is full'} color={"primary"}/>}
-      </header>
-      <label className="switch" onChange={toggleChange}>
-        <input type="checkbox"></input>
+      <label className="switch" >
+        <input type="checkbox" value={isActive} onChange={toggleChange}></input>
         <span className="slider round"></span>
       </label>
       {
         isActive === true ?
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="flex-start"
-          >
-            <Card style={{width: 500, margin: 20}}>
-              <CardMedia
-                style={{width: "100%", height: 250}}
-                image={currentImage}
-                title="Contemplative Rephttps://www.drronsanimalhospitalsimivalley.com/wp-content/uploads/2018/11/Happy-Cat-Eating-Food.jpgtile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Live measure
-                </Typography>
-                <XYPlot height={300} width={500} xType="time"
-                        yDomain={[0, 1024]}
-                >
-
-                  <VerticalGridLines/>
-                  <HorizontalGridLines/>
-                  <XAxis/>
-                  <YAxis/>
-                  <MarkSeries data={datas}/>
-
-                </XYPlot>
-              </CardContent>
-            </Card>
-            <Card style={{width: 500, margin: 20}}>
-
-              <XYPlot height={300} width={500}
-                      stroke="#f70"
-                      style={{strokeWidth: 5}}>
-                {/*<LineMarkSeries data={datas} color={"red"}/>*/}
-                <LineSeries data={datas} color="#ba4fb9" style={{fill: "none"}}/>
-              </XYPlot>
-            </Card>
-          </Grid>
+          <div className="container">
+            <ul>
+              {dataList.map((data, index) => (
+                <li key={index}>{data}</li>
+              ))}
+            </ul>
+          </div>
           :
         <p> Y'a rien</p>
       }
